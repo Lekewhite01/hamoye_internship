05134e831ce69ad410fbfef7877350ec2c39cdf1
Olorunleke Akindote <olorunleke.akindote@Terra-139.local>
2023-11-01 19:43:59 +0100

    modified app script

--- premiere_project/serve/app.py
+++ premiere_project/serve/app.py
@@ -141,7 +141,8 @@ def main():
             # Make predictions using the loaded model and display the result
             prediction = loaded_model.predict(input_df)
             st.success('Inference Done!')
-            st.write(f"This species belongs in class: {prediction}")
+            st.header("Predicted Class")
+            st.write(prediction)
 
     if prediction_type == 'Batch Prediction':
         # If the user selects 'Batch Prediction', allow them to upload a CSV file
