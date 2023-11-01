17b4d88263c973bb2b66624fabe5a7dbdfd687c6
Olorunleke Akindote <olorunleke.akindote@Terra-139.local>
2023-11-01 17:26:56 +0100

    modified path to model

--- premiere_project/serve/app.py
+++ premiere_project/serve/app.py
@@ -72,7 +72,7 @@ st.warning(f"You have selected: {prediction_type}")
 
 # Specify model path
 # This is for deployment
-model_path = Path(__file__).parents[1] / 'model/model_2023-10-30T14:33:01.425548.pkl'
+model_path = Path('premiere_project/serve/model/model_2023-10-30T14:33:01.425548.pkl')
 
 # This is for local runs
 # model_path = 'model/model_2023-10-30T14:33:01.425548.pkl'
